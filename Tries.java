94484cb06233199278d06cda65222457399237bc
tengn <34149657+tengn@users.noreply.github.com>
2020-06-11 22:39:16 -0400

    Create Tries.java

---
+++ Tries.java
@@ -0,0 +1,42 @@
+public class Tries {
+	private class TriesNode {
+Map<Character, Tries> chlidren;
+boolean isEnd;
+Character value;
+}
+
+private TriesNode root
+public Tries() {
+	root = new TriesNode();
+}
+
+private void setValue() {
+
+}
+
+private void setIslast() {
+
+}
+
+
+public void insert(String s) {
+	char last = 'a';
+	for (char c : s.toCharArray()) {
+		if (root.chlidren.size() == 0) {
+			// no root node yet, put this
+			// as root node
+		} else {
+			if (!root.chlidren.containsKey(c)) {
+				TriesNode temp = new TriesNode();
+				temp.setValue(c);
+				root.chlidren.put(c, temp);
+			}
+		}
+		last = c;
+	}
+	TriesNode temp = new TriesNode();
+	temp.setIslast(True);
+	last.chlidren.put("", temp);
+}
+
+}
